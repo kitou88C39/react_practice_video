587e40cf7537177e41b77f053d15998da2f67709
kitou88C39 <kanfutrooper@icloud.com>
2023-10-09 15:09:42 +0900

    pages/TodoList.jsx 編集

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 
 export const Todo = () => {
   //todoがデータ、setTodoが変更するための関数
+  //useStateの()の中は、デフォルトの値が入るので、todoの中に筋トレが入るので、inputの中に筋トレが表示される
   const [todo, setTodo] = useState('筋トレ');
   return (
     <>
       <div>TODO</div>
-      <input type='text' value={todo} />
+      <input
+        type='text'
+        value={todo}
+        onChange={(event) => {
+          //setTodoを使って変更する値を入れる
+          setTodo(event.target.value);
+        }}
+      />
     </>
   );
 };
